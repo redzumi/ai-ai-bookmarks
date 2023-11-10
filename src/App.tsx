1adc5782c4595d1415b2f721709f9273b47597b4
Evgenii Bykovskikh <redzumi.tao@gmail.com>
2023-11-10 17:12:23 +0300

    Done with read bookmarks

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+type Bookmark = chrome.bookmarks.BookmarkTreeNode;
+
+const readBookmarks = (tree: Bookmark[], items: Bookmark[]) => {
+  tree.forEach((item) => {
+    if (item.children) {
+      readBookmarks(item.children, items);
+    } else {
+      items.push(item);
+    }
+  });
+
+  return items;
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [bookmarks, setBookmarks] = useState<Bookmark[]>([]);
+
+  useEffect(() => {
+    chrome?.bookmarks?.getTree((tree) => {
+      const items = readBookmarks(tree, []);
+      setBookmarks(items);
+    });
+  }, []);
 
   return (
     <>
       <div>
+        {bookmarks.map((bookmark) => (
+          <div>{bookmark.title}</div>
+        ))}
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -29,7 +54,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;

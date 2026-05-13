bceabb6cd6de93e84fe658811cd7fbeade214288
Evgenii Bykovskikh <redzumi.tao@gmail.com>
2026-05-13 18:01:18 +0300

    Update manifest to remove module type from background service worker, add vite-plugin-zip-pack to package.json, and configure Vite to include zip plugin for packaging the build.

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { crx } from "@crxjs/vite-plugin";
+import zip from "vite-plugin-zip-pack";
 import manifest from "./manifest.json";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), crx({ manifest })],
+  plugins: [
+    react(), 
+    crx({ manifest }), 
+    zip({
+      outDir: "release", // Folder where the zip will be saved
+      outFileName: "bundle.zip", // Name of the final package
+    }),
+  ],
   build: {
     rollupOptions: {
       input: "index.html",
